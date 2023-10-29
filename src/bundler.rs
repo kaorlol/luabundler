ec1d430783e4de71796d5a9e17e27b89e35a1954
Kaoru <96850544+kaorlol@users.noreply.github.com>
2023-10-29 02:07:18 -0400

    minor shit

--- src/bundler.rs
+++ src/bundler.rs
@@ -1,14 +1,15 @@
 use std::{
     error::Error,
-    path::PathBuf
+    path::PathBuf,
+    cmp::min
 };
 
 use tokio::{
     fs::{
-        read_to_string,
-        File
+        File,
+        read_to_string
     },
-    io::copy,
+    io::AsyncWriteExt,
     time::Instant
 };
 
@@ -21,9 +22,8 @@ use darklua_core::{
 
 use colored::Colorize;
 use stacker::maybe_grow;
-
-// Regex on top. Just understand it tbh.
 use regex::Regex;
+use async_recursion::async_recursion;
 
 const REQUIRE_PATTERNS: &[&str] = &[
     // require("module.lua",...) : 'require("module.lua",...)'
@@ -39,6 +39,7 @@ const IN_STRING_PATTERN: &str = r#"^['"](.+)['"]$"#;
 // Matches comments: --, --[[ ]], --[=[ ]=]
 const IN_COMMENT_PATTERN: &str = r#"--\[=*\[[\s\S]*?\]=*\]|['"]*--\s*.*['"]?"#;
 
+// Removes comments from a file
 async fn remove_comments(contents: &str) -> Result<String, Box<dyn Error>> {
     // Create a regex instance for the comment pattern
     let re: Regex = Regex::new(IN_COMMENT_PATTERN)?;
@@ -58,9 +59,10 @@ async fn remove_comments(contents: &str) -> Result<String, Box<dyn Error>> {
 }
 
 // Recursively parses a file for require calls, and returns a vector of (require, args) tuples
-#[async_recursion::async_recursion]
+#[async_recursion]
 async fn parse_file(path: &str) -> Result<Vec<(String, String, String, String)>, Box<dyn Error>> {
-    let contents = remove_comments(&read_to_string(path).await?).await?;
+    let mut require_path = PathBuf::from(path);
+    let contents = remove_comments(&read_to_string(require_path.clone()).await?).await?;
     let mut calls = Vec::new();
 
     for pattern in REQUIRE_PATTERNS {
@@ -68,10 +70,9 @@ async fn parse_file(path: &str) -> Result<Vec<(String, String, String, String)>,
         for cap in regex.captures_iter(&contents) {
             let matched = cap.get(0).unwrap().as_str().trim().to_string();
             let require = cap.get(1).unwrap().as_str().trim().to_string();
-            let args = cap.get(2).map(|m| m.as_str().trim().to_string()).unwrap_or_default();
+            let args = cap.get(2).map_or(String::new(), |m| m.as_str().trim().to_string());
             let func_args = cap.get(3).map_or(String::new(), |m| m.as_str().trim().to_string());
 
-            let mut require_path = PathBuf::from(path);
             require_path.pop();
             require_path.push(&require);
 
@@ -90,21 +91,18 @@ async fn parse_file(path: &str) -> Result<Vec<(String, String, String, String)>,
 // Replaces all require calls in a file with the contents of the file at the given path
 async fn replace_requires(origin: &str, requires: Vec<(String, String, String, String)>) -> Result<String, Box<dyn Error>> {
     let origin_buf = PathBuf::from(origin);
-    let main_dir = origin_buf.parent().unwrap().to_str().unwrap();
+    let main_buf = PathBuf::from(origin_buf.parent().unwrap().to_str().unwrap());
     let mut replaced_contents = remove_comments(&read_to_string(&origin_buf).await?).await?;  // Initialize with the original content
 
     for (mut matched, require, args, func_args) in requires {
-        let require_path = PathBuf::from(main_dir).join(&require);
+        let require_path = main_buf.join(&require);
         let contents = read_to_string(&require_path).await?;
 
         // Check if the first and last characters are either ' or "
         let in_string_regex = Regex::new(IN_STRING_PATTERN)?;
         if in_string_regex.is_match(&matched) {
             // Replace the first and last characters with [[ and ]]
-            let mut replaced = String::from("[[");
-            replaced.push_str(&matched[1..matched.len() - 1]);
-            replaced.push_str("]]");
-
+            let replaced = format!("[[{}]]", &matched[1..matched.len() - 1]);
             replaced_contents = replaced_contents.replace(&matched, &replaced);
 
             // Remove the first and last string in matched
@@ -116,7 +114,8 @@ async fn replace_requires(origin: &str, requires: Vec<(String, String, String, S
         let mut replaced = format!("(function(...)\n{}\nend)({});", contents, args);
 
         if !func_args.is_empty() {
-            replaced.pop(); // Remove the last semicolon
+            // Remove the last semicolon and add func_args
+            replaced.pop();
             replaced.push_str(&func_args);
         }
 
@@ -152,6 +151,21 @@ fn process_code(buffer: PathBuf, minify: bool) {
     });
 }
 
+
+// Writes the data to the file in chunks of the given size
+async fn write_in_chunks(file_path: &str, data: &[u8], chunk_size: usize) -> Result<(), Box<dyn Error>> {
+    let mut file = File::create(file_path).await?;
+    let mut offset = 0;
+
+    while offset < data.len() {
+        let end = min(offset + chunk_size, data.len()); // Calculate the end of the chunk
+        file.write_all(&data[offset..end]).await?; // Write the chunk
+        offset += chunk_size; // Increment the offset
+    }
+
+    Ok(())
+}
+
 // Bundles the given file and writes the bundled code to the output file
 pub async fn bundle(main_path: &str, bundle_path: &str, _minify: bool, noprocess: bool) -> Result<(), Box<dyn Error>> {
     let start = Instant::now();
@@ -161,8 +175,7 @@ pub async fn bundle(main_path: &str, bundle_path: &str, _minify: bool, noprocess
     let bundled = replace_requires(main_path, calls).await?;
 
     // Write the bundled code to the output file
-    let mut file = File::create(bundle_path).await?;
-    copy(&mut bundled.as_bytes(), &mut file).await?;
+    write_in_chunks(bundle_path, bundled.as_bytes(), 1024 * 1024).await?;
 
     println!("{}", format!("{} {} {}", "Bundled".blue(), main_path, format!("in {:?}", start.elapsed()).dimmed()));
 
